79e2c6009b4a322ef4f8bf80334587459adcb64e
Narmeen Massalha <narmeenmassalha4975@gmail.com>
2018-11-26 18:54:54 +0200

    seed file

---
+++ db/migrate/20181126143439_change_name.rb
@@ -0,0 +1,4 @@
+class ChangeName < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+
+user = User.new(
+	first_name:"Zaneta",
+	last_name: "Uba",
+	email: "Uba@gmail.com",
+	password: "123456"
+	)
+user.save!
+
+goal1 = Goal.new(
+	name: "House"
+	)
+goal1.save!
+
+
+goal2 = Goal.new(
+	name: "Car"
+	)
+goal2.save!
+
+
+goal3 = Goal.new(
+	name: "Education"
+	)
+goal3.save!
+
+goal4 = Goal.new(
+    name: "Wedding"
+	)
+goal4.save!
+
+
+goal5 = Goal.new(
+	name: "Travel"
+	)
+goal5.save!
+
+
+goal6 = Goal.create!(
+	name: "General Saving"
+	)
+Budget.new(
+	user_id: user.id,
+	name: "food",
+	amount_cents: 10000,
+	category_list:["Food & Drink","Bills & Fees", "Car", "Education", "Transport", "Clothes", "Home", "Entertainment","Healthcare"]
+
+	)
+
