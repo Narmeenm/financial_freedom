2d70f1fc178e98a40c3fb64c3d2460a255d728ca
zaneta uba <zaneta.uba@gmail.com>
2018-11-27 19:03:44 +0200

    calendar done plus some colors etc

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,9 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/themes/material_green.css" // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+
+
+flatpickr("#range_start", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+})
