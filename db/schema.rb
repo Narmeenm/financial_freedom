ef2ed4ab60f03b33f945f485669fe99364116320
Narmeen Massalha <narmeenmassalha4975@gmail.com>
2018-11-28 14:41:16 +0200

    cash flow show,new,index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_134228) do
+ActiveRecord::Schema.define(version: 2018_11_28_093119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2018_11_27_134228) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
-    t.boolean "income", default: true
     t.index ["budget_id"], name: "index_cash_flows_on_budget_id"
     t.index ["user_id"], name: "index_cash_flows_on_user_id"
   end
