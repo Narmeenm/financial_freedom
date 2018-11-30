23839d192efd8a8a054ca19760cb6fb8e0b6baaa
Cory Adelman <cadelman27@gmail.com>
2018-11-30 14:20:36 +0200

    routes and some CSS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,27 @@
 class UsersController < ApplicationController
- 
+
  def salary
     # @user.salary = current_user.salary
-    @user = current_user 
+    @user = current_user
  end
 def save_salary
-    @user = current_user 
+    @user = current_user
 
 	@user.update(salary_params)
 	redirect_to dashboard_path
-	
+
 end
 
+def balance
+user = current.user
+end
 def dashboard
    @budgets = current_user.budgets
    @budgets = Budget.where(user_id: current_user.id)
-end	
+end
 
 
  def salary_params
     params.require(:user).permit(:salary,:balance)
- end	
+ end
 end
