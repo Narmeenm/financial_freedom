c48c8a3048a06ed137547a795d8fbf56ae983cdb
zaneta uba <zaneta.uba@gmail.com>
2018-12-03 10:43:39 +0200

    redirection if there is no budgets

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,7 +2,7 @@ class Budget < ApplicationRecord
   belongs_to :user
   belongs_to :category
   monetize :amount_cents
-  acts_as_taggable_on :categories
-  
+
   has_many :cash_flows ,dependent: :destroy
+  validates_uniqueness_of :category_id, scope: :category
 end
